c31f42f96645e55fb95d0280fe8f618e4cb4c254
Neil Wang <neil.wang@10data.com>
2014-04-29 18:24:19 +0800

    finished checkout

--- controllers/sportsstore.js
+++ controllers/sportsstore.js
@@ -1,6 +1,7 @@
 angular.module('sportsStore')
     .constant('dataUrl', 'http://products7.apiary-mock.com/products')
-    .controller('sportsStoreCtrl', function ($scope, $http, dataUrl) {
+    .constant('orderUrl', 'http://products7.apiary-mock.com/order')
+    .controller('sportsStoreCtrl', function ($scope, $http, $location, dataUrl, orderUrl, cart) {
         $scope.data = {};
 
         $http.get(dataUrl)
@@ -11,4 +12,21 @@ angular.module('sportsStore')
                 $scope.data.error = err;
                 console.warn(err);
             });
+
+        $scope.sendOrder = function (shippingDetails) {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+
+            $http.post(orderUrl, order)
+                .success(function (data) {
+                    $scope.data.orderId = data.id;
+                    cart.getProducts().length = 0;
+                })
+                .error(function (err) {
+                    $scope.data.orderError = err;
+                })
+                .finally(function () {
+                    $location.path('/complete');
+                });
+        };
     });
